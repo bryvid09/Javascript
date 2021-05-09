bff13ed0531c162c80f918ab1c0ba2daadbd032a
Brayan <btipan.daw2n@lamerce.com>
2021-05-10 01:08:07 +0200

    rooms

--- Uf4/Pong/public/index.js
+++ Uf4/Pong/public/index.js
@@ -6,12 +6,12 @@ var room = [];
 var socket = io.connect('http://localhost:8080', {
     'forceNew': true
 });
+
 socket.on('getConexiones', function (data) {
     conexiones = data;
     if (!jugador) {
         jugador = conexiones % 2 != 0 ? new Jugador(1) : new Jugador(2); //Controlamos que no se pise un jugador con otro            
-        jugador.id = socket.id;
-        room.push(jugador);
+        jugador.id = socket.id;        
         socket.emit('start', jugador);
     }
 });
@@ -23,21 +23,6 @@ socket.on('room', function (data) {
                 room = data[i];
             }
         }       
-    }
+    }    
     console.log(room);
 });
-
-window.onload = function () {
-
-
-
-
-
-
-    document.onkeydown = function (ev) {
-        if (ev.keyCode == 83 && jugadores.length == 2 && !juego) {
-            juego = new Juego(jugador);
-            console.log(jugadores.length);
-        }
-    }
-}
